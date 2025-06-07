bf563d251808a6703c459b309ae49d75a65429c6
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:32:29 -0500

    El constructor recibe los tres datos básicos.

--- src/main/java/ejercicio1_animales/Animal.java
+++ src/main/java/ejercicio1_animales/Animal.java
@@ -5,6 +5,11 @@ public class Animal {
     private String tipo; // terrestre, aéreo, acuático
     private String genero; // masculino, femenino
 
+    public Animal(String nombre, String tipo, String genero) {
+        this.nombre = nombre;
+        this.tipo = tipo.toLowerCase();
+        this.genero = genero.toLowerCase();
+    }
 
 
 }
