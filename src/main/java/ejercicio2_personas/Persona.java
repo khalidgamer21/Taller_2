44f3d9cbc657f55b2ad44f6ccb94cee0b04c62ce
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:58:03 -0500

    creo el constructor

--- src/main/java/ejercicio2_personas/Persona.java
+++ src/main/java/ejercicio2_personas/Persona.java
@@ -7,4 +7,15 @@ public class Persona {
     private String genero; // M o F
     private double sueldoHora;
     private String cargo;
+
+    public Persona(String nombre, String apellido, int edad, String genero, double sueldoHora, String cargo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.genero = genero.toUpperCase();
+        this.sueldoHora = sueldoHora;
+        this.cargo = cargo;
+    }
+
+
 }
