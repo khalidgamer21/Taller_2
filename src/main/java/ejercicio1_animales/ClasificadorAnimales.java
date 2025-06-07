6dc4dfa443eaccfb52df827251f18a93f4281d3e
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:36:58 -0500

    Cree un TreeMap para agrupar los animales por tipo ("Terrestres", "Aéreos", etc.).
    Se ordenará automáticamente por clave (alfabéticamente).

--- src/main/java/ejercicio1_animales/ClasificadorAnimales.java
+++ src/main/java/ejercicio1_animales/ClasificadorAnimales.java
@@ -5,6 +5,7 @@ public class ClasificadorAnimales {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Animal> animales = new ArrayList<>();
+        Map<String, List<Animal>> clasificacion = new TreeMap<>();
 
     }
 }
