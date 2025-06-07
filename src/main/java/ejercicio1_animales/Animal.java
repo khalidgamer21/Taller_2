dd5f319cd97d270882f0df0ad18c8ddfcb0144f9
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:31:37 -0500

    Creo la estructura del objeto Animal con sus atributos privados

--- src/main/java/ejercicio1_animales/Animal.java
+++ src/main/java/ejercicio1_animales/Animal.java
@@ -1,4 +1,10 @@
 package ejercicio1_animales;
 
 public class Animal {
+    private String nombre;
+    private String tipo; // terrestre, aéreo, acuático
+    private String genero; // masculino, femenino
+
+
+
 }
