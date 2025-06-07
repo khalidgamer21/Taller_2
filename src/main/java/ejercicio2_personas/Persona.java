e6f0c9d2c4e9e2af4714347e57db8c3436f2911b
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:57:34 -0500

    creo los atributos del objeto persona

--- src/main/java/ejercicio2_personas/Persona.java
+++ src/main/java/ejercicio2_personas/Persona.java
@@ -1,4 +1,10 @@
 package ejercicio2_personas;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero; // M o F
+    private double sueldoHora;
+    private String cargo;
 }
