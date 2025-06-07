29c8e69b3faf24538ea0982dfe64c9c2c5886472
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:59:14 -0500

    Métodos para acceder o modificar cada atributo.

--- src/main/java/ejercicio2_personas/Persona.java
+++ src/main/java/ejercicio2_personas/Persona.java
@@ -16,6 +16,20 @@ public class Persona {
         this.sueldoHora = sueldoHora;
         this.cargo = cargo;
     }
+    // Getters
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public int getEdad() { return edad; }
+    public String getGenero() { return genero; }
+    public double getSueldoHora() { return sueldoHora; }
+    public String getCargo() { return cargo; }
 
+    // Setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public void setGenero(String genero) { this.genero = genero; }
+    public void setSueldoHora(double sueldoHora) { this.sueldoHora = sueldoHora; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
 
 }
