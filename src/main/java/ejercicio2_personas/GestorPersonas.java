55442c5c5f0ce527c2f838fbf42f2542c02bf511
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 21:02:37 -0500

    Creo Scanner para leer desde consola.
    Lista de personas almacenadas.

--- src/main/java/ejercicio2_personas/GestorPersonas.java
+++ src/main/java/ejercicio2_personas/GestorPersonas.java
@@ -1,4 +1,10 @@
 package ejercicio2_personas;
+import java.util.*;
+
 
 public class GestorPersonas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Persona> personas = new ArrayList<>();
+    }
 }
