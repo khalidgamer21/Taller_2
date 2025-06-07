8f75b1353280f08d4f7dc24fa2273e58971cf4b0
marlon <marlon.monterrosa.m@gmail.com>
2025-06-06 20:33:23 -0500

    Los getters/setters permiten leer/modificar atributos si es necesario

--- src/main/java/ejercicio1_animales/Animal.java
+++ src/main/java/ejercicio1_animales/Animal.java
@@ -11,5 +11,34 @@ public class Animal {
         this.genero = genero.toLowerCase();
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo.toLowerCase();
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
 
 }
